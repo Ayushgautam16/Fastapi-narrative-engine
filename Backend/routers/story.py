f6f06505f4ed51d0cce06d632ed0ffd0988304d9
Master Ayush Gautam <ayushgautam2007.kg@gmail.com>
2026-04-17 13:58:38 +0530

    inhance code readability in story.py

--- Backend/routers/story.py
+++ Backend/routers/story.py
@@ -14,6 +14,7 @@ from schemas.story import (
     CompleteStoryResponse, CompleteStoryNodeResponse, CreateStoryRequest
 )
 from schemas.job import StoryJobResponse
+
 from core.story_generator import StoryGenerator
 
 router = APIRouter(
